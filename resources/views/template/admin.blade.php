b7f83a50fea7ab1cf1e7fb13200fa70547dc0295
Chanachai Puttaruksa <chanachai_mig@hotmail.com>
2021-03-15 13:25:16 +0700

    change admin container

--- resources/views/template/admin.blade.php
+++ resources/views/template/admin.blade.php
@@ -45,15 +45,15 @@
 
             <div id="page-content-wrapper">
                 <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom">
-                    <div class="container">
+                    <div class="container-fluid">
                         <button class="btn btn-primary" id="menu-toggle">
                             <i class="fa fa-indent"></i>
                         </button>
-    
+
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-    
+
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
                                 {{-- <li class="nav-item active">
