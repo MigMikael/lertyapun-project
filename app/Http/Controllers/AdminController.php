9a9332a4ec6b49e663b3ab7e4862abdfa77ab319
Chanachai Puttaruksa <chanachai_mig@hotmail.com>
2021-04-16 02:05:54 +0700

    add admin dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Customer;
+use App\Models\Order;
 
 class AdminController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $customerPending = Customer::where('status', 'pending')->count();
+        $orderPending = Order::where('status', 'pending')->count();
+        $orderPayment = Order::where('status', 'payment')->count();
+        return view('admin.dashboard', [
+            'customerPending' => $customerPending,
+            'orderPending' => $orderPending,
+            'orderPayment' => $orderPayment,
+        ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,9 +2,45 @@
 
 @section('content')
 <div class="admin-container">
-    <h3 class="mt-4">Dashboard</h3>
-    <hr>
     <h1 class="mt-4">LERTYAPHAN Store</h1>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <hr>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card bg-light text-dark">
+                <div class="card-header">
+                    <h4>ลูกค้ารออนุมัติ</h4>
+                </div>
+                <div class="card-body">
+                    <a href="{{ url('admin/customers') }}">
+                        <h3>{{ $customerPending }} คน</h3>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card bg-light text-dark">
+                <div class="card-header">
+                    <h4>คำสั่งซื้อรออนุมัติ</h4>
+                </div>
+                <div class="card-body">
+                    <a href="{{ url('admin/orders') }}">
+                        <h3>{{ $orderPending }} คำสั่งซื้อ</h3>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card bg-light text-dark">
+                <div class="card-header">
+                    <h4>คำสั่งซื้อรอยืนยันเงินเข้า</h4>
+                </div>
+                <div class="card-body">
+                    <a href="{{ url('admin/orders') }}">
+                        <h3>{{ $orderPayment }} คำสั่งซื้อ</h3>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
