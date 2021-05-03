b4b5fc4ea984caaef6bee41aa43a77ab6ed41da5
Aphinan Peerachaidacho <aphinan.p@zipeventapp.com>
2021-05-03 18:30:49 +0700

    Add query string to the css file for clear cache version

--- resources/views/template/customer.blade.php
+++ resources/views/template/customer.blade.php
@@ -15,8 +15,8 @@
         <link href="{{ URL::asset('vendor/simple-line-icons/css/simple-line-icons.css') }}" rel="stylesheet" type="text/css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
-        <link href="{{ URL::asset('css/landing-page.css') }}" type="text/css" rel="stylesheet">
-        <link href="{{ URL::asset('css/app.css') }}" type="text/css" rel="stylesheet">
+        <link href="{{ URL::asset('css/landing-page.css') }}?v=<?=time()?>" type="text/css" rel="stylesheet">
+        <link href="{{ URL::asset('css/app.css') }}?v=<?=time()?>" type="text/css" rel="stylesheet">
         <script src="{{ URL::asset('vendor/jquery/jquery.min.js') }}"></script>
 
         @yield('head')
