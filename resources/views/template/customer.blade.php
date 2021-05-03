cea7df1fe28a821c4341e2306f0af19f45a2cc0b
Aphinan Peerachaidacho <aphinan.p@zipeventapp.com>
2021-05-03 17:29:17 +0700

    Add mime type for css

--- resources/views/template/customer.blade.php
+++ resources/views/template/customer.blade.php
@@ -8,15 +8,15 @@
 
         <title>@yield('title', 'LERTYAPHAN')</title>
 
-        <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ URL::asset('css/bootstrap.min.css') }}" type="text/css" rel="stylesheet">
         <link rel="shortcut icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon">
         <link rel="icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon">
-        <link href="{{ URL::asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+        <link href="{{ URL::asset('vendor/fontawesome-free/css/all.min.css') }}" type="text/css" rel="stylesheet">
         <link href="{{ URL::asset('vendor/simple-line-icons/css/simple-line-icons.css') }}" rel="stylesheet" type="text/css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
-        <link href="{{ URL::asset('css/landing-page.css') }}" rel="stylesheet">
-        <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ URL::asset('css/landing-page.css') }}" type="text/css" rel="stylesheet">
+        <link href="{{ URL::asset('css/app.css') }}" type="text/css" rel="stylesheet">
         <script src="{{ URL::asset('vendor/jquery/jquery.min.js') }}"></script>
 
         @yield('head')
