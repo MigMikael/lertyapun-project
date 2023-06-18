18694ed7726990f90199e5b3e21afaa075a8bae1
Aphinan Peerachaidacho <aphinan.pee@gmail.com>
2023-06-18 11:05:17 +0700

    Fix default text in error modal

--- resources/views/customer/_errorModal.blade.php
+++ resources/views/customer/_errorModal.blade.php
@@ -3,12 +3,13 @@
         <div class="modal-content">
             <!-- Modal Header -->
             <div class="modal-header">
-                <h4 class="modal-title" id="title">Error</h4>
+                <h4 class="modal-title" id="title">พบข้อผิดพลาด</h4>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
             <!-- Modal body -->
             <div class="modal-body" id="message">
-                Modal body..
+                พบข้อผิดพลาดบางประการ กรุณา refresh หน้าจอใหม่ <br>
+                หากยังพบปัญหากรุณาติดต่อ โทร 095-254-4525
             </div>
             <!-- Modal footer -->
             <div class="modal-footer">
